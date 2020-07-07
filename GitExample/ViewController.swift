512b42c448cfc79518b284080b68807389615ed8
KrisGroll <Kristina-gorelikova@mail.ru>
2020-07-07 12:56:02 +0200

    Initial Commit

---
+++ GitExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitExample
+//
+//  Created by KrisGroll on 07.07.2020.
+//  Copyright © 2020 KrisGroll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
